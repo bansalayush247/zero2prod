264b829c65ba1462362000a203a7e63139cbb047
bansalayush247 <bansalayush24704@gmail.com>
2025-12-17 11:38:09 +0530

    Implement security audit workflow, create subscriptions table, initialize database script, and set up application configuration
    - Added a GitHub Actions workflow for security audits using `cargo deny`.
    - Created a SQL migration file to establish a `subscriptions` table with necessary fields.
    - Developed an initialization script for the database, including Docker setup and migration execution.
    - Defined application configuration structures for database settings and implemented a function to load configurations from a YAML file.
    - Set up routing for health check and subscription endpoints in the Actix web application.
    - Implemented subscription handling logic to insert data into the `subscriptions` table.
    - Created tests for health check and subscription endpoints, including database setup and teardown.

--- target/debug/deps/zero2prod-9b15f1395aef2bce.d
+++
@@ -1,5 +0,0 @@
-/home/nightfury69/data/zero2prod/target/debug/deps/zero2prod-9b15f1395aef2bce.d: src/main.rs
-
-/home/nightfury69/data/zero2prod/target/debug/deps/libzero2prod-9b15f1395aef2bce.rmeta: src/main.rs
-
-src/main.rs:
